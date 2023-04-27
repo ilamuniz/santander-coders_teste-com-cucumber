c0883aee9bde065860a7a260eb0063f3bb1955f7
IlaMuniz <ilammuniz@gmail.com>
2023-04-27 00:54:44 -0300

    Criação das classes User, Task e Status

---
+++ src/test/java/br/ada/helloworldcucumber/model/Status.java
@@ -0,0 +1,7 @@
+package br.ada.helloworldcucumber.model;
+
+public enum Status {
+
+    OPEN, CLOSE;
+
+}
---
+++ src/test/java/br/ada/helloworldcucumber/model/Task.java
@@ -0,0 +1,52 @@
+package br.ada.helloworldcucumber.model;
+
+import java.time.LocalDate;
+
+public class Task {
+
+    private String title;
+    private String description;
+    private User user;
+    private Status status;
+    private LocalDate closedAt;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public LocalDate getClosedAt() {
+        return closedAt;
+    }
+
+    public void setClosedAt(LocalDate closedAt) {
+        this.closedAt = closedAt;
+    }
+}
